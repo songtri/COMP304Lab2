5f4b0f615e2e91240dfb2ec50f38b8db0d98f293
Song Wook <songtri@gmail.com>
2020-10-12 22:36:04 -0400

    Project setup

---
+++ app/src/main/java/wook/song/s301047817/wookActivity.java
@@ -0,0 +1,15 @@
+package wook.song.s301047817;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/* Wook Song - 301047817 - Section 2 */
+public class wookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
